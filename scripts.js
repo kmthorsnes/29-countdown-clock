7b23e3cc0facf31614d947ae2ef74480d166df72
kalle <kmthorsnes@gmail.com>
2018-06-20 21:23:56 +0200

    adds timestamp for when to be back

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
     
    Countdown = setInterval(()=> {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -22,5 +25,15 @@ function displayTimeLeft(seconds) {
     const remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0': '' }${remainderSeconds}`;
     timerDisplay.textContent = display;
+    document.title = display; 
     console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();  
+    endTime.textContent = `Be back at ${hours}:${minutes}`;
+
+
 }
